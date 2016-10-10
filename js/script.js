60315d0a33357fdfc5f4057cc3ac576d8962ff7b
Artem Arshaniy <artem.arshaniy108@gmail.com>
2016-10-10 20:21:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  var $links = $('.menu a');
+
+  $links.on('click', function (e) {
+    var $submenu =  $(this).siblings('.submenu');
+    e.preventDefault();
+    $submenu.slideToggle();
+  });
+});
